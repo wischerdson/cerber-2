9bae085685a099fc908880664f8868e27b1a04fc
Осмухин Даниил <wischerdson@gmail.com>
2024-06-10 22:13:47 +0300

    Redesign search field & autofocus on typing detection

--- client/components/account/Search.vue
+++ client/components/account/Search.vue
@@ -1,6 +1,17 @@
 <template>
-	<TextField class="!pl-9 !h-10" placeholder="Поиск" v-model="searchQuery" disable-label>
-		<template #layer-above-field>
+	<div class="search relative" :class="{ focused }">
+		<input
+			class="search-field pl-9 h-10"
+			type="text"
+			v-uid
+			autocomplete="off"
+			placeholder="Поиск"
+			v-model="searchQuery"
+			ref="$input"
+			@focus="focused = true"
+			@blur="focused = false"
+		>
+		<div class="absolute inset-0 pointer-events-none">
 			<div class="h-full flex justify-between items-center pl-2 pr-4">
 				<icon class="text-black/50 dark:text-white/50" size="22px" name="ph:magnifying-glass" />
 
@@ -14,22 +25,51 @@
 					</Clickable>
 				</transition>
 			</div>
-		</template>
-	</TextField>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
 
-import TextField from '~/components/ui/TextField.vue'
 import Clickable from '~/components/ui/Clickable.vue'
-import { ref, computed } from '#imports'
+import { ref, computed, onMounted, onUnmounted } from '#imports'
+import { useTypingDetector } from '~/composables/use-typing-detector';
 
 const searchQuery = ref('')
 const showClearBtn = computed(() => !!searchQuery.value.length)
+const focused = ref(false)
+const $input = ref<HTMLInputElement>()
+let stopTypingDetection: () => void
+
+onMounted(() => {
+	stopTypingDetection = useTypingDetector(() => $input.value?.focus())
+})
+
+onUnmounted(() => stopTypingDetection())
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
+
+.search {
+	transition: transform .3s ease;
+
+	&:hover, &.focused {
+		transform: scale(1.028);
+
+		.search-field {
+			box-shadow: 0 5px 16px 0 rgba(0, 0, 0, 0.1);
+		}
+	}
+}
+
+.search-field {
+	width: 100%;
+	background-color: #fff;
+	border-radius: 10px;
+	box-shadow: 0 5px 16px 0 rgba(0, 0, 0, 0.05);
+	transition: box-shadow .3s ease;
+}
 
 .clear-search-btn {
 	opacity: .5;
@@ -44,7 +84,6 @@ const showClearBtn = computed(() => !!searchQuery.value.length)
 	}
 
 	&:hover {
-		// background-color: darken(#fff, 14%);
 		opacity: 1;
 	}
 }
