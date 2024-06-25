2c7e8a4962cabfc0b16bf3e83547d3e4b68cb36f
Осмухин Даниил <wischerdson@gmail.com>
2024-06-25 22:58:08 +0300

    Secret and group lists

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,20 +1,108 @@
 <template>
 	<DefaultPageView>
-		<!-- <div class="flex justify-between">
-			<h1 class="font-bold text-gray-900 dark:text-gray-100 text-2.5xl">Доступы</h1>
-			<div>
-				<TheClickable
-					class="add-password-btn bg-gray-100 text-black flex items-center justify-center h-10 px-4 rounded-full"
-				>
-					<icon class="mr-2" size="24px" name="material-symbols:add-rounded" />
-					<span>Добавить</span>
-				</TheClickable>
-			</div>
-		</div> -->
-
 		<div class="flex items-start gap-6 mt-4">
 			<Sidebar />
-			<ContentTile class="w-full h-52"> </ContentTile>
+			<ContentTile class="w-full">
+				<div>
+					<TheClickable class="flex items-center justify-center h-9 text-gray-700 px-4 rounded-full -ml-3">
+						<span class="text-sm">Доступы</span>
+						<icon
+							class="chevron-right -mb-0.5 ml-1 -mr-2 rotate-90"
+							name="material-symbols:chevron-right-rounded"
+							size="22px"
+						/>
+					</TheClickable>
+					<ul class="">
+						<li class="secret-item flex items-center space-x-3 rounded-lg py-1.5 px-2 -mx-1.5">
+							<div class="secret-icon w-8 h-8 flex items-center justify-center bg-gray-50 rounded-lg">
+								<icon class="text-gray-700" name="material-symbols:database" size="22px" />
+							</div>
+							<div>
+								<div>
+									<span>База данных</span>
+								</div>
+							</div>
+						</li>
+						<li class="secret-item flex items-center space-x-3 rounded-lg py-1.5 px-2 -mx-1.5">
+							<div class="w-8 h-8 flex items-center justify-center bg-gray-50 rounded-lg">
+								<icon class="text-gray-700" name="material-symbols:terminal-rounded" size="22px" />
+							</div>
+							<div>
+								<div>
+									<span>SSH</span>
+								</div>
+							</div>
+						</li>
+						<li class="secret-item flex items-center space-x-3 rounded-lg py-1.5 px-2 -mx-1.5">
+							<div class="w-8 h-8 flex items-center justify-center bg-gray-50 rounded-lg">
+								<span class="text-gray-700 font-medium text-xl select-none">A</span>
+							</div>
+							<div>
+								<div>
+									<span>Админка</span>
+								</div>
+							</div>
+						</li>
+						<li class="secret-item flex items-center space-x-3 rounded-lg py-1.5 px-2 -mx-1.5">
+							<div class="w-8 h-8 flex items-center justify-center  rounded-lg ">
+								<img src="https://panteric.ru/files/gallery/58/mstile_144x144/favikon_1521468371_1645088044.png" />
+							</div>
+							<div>
+								<div>
+									<span>Админка</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+
+				<hr class="my-5 border-gray-100">
+
+				<div>
+					<TheClickable class="flex items-center justify-center h-9 text-gray-700 px-4 rounded-full -ml-3">
+						<span class="text-sm">Группы</span>
+						<icon
+							class="chevron-right -mb-0.5 ml-1 -mr-2 rotate-90"
+							name="material-symbols:chevron-right-rounded"
+							size="22px"
+						/>
+					</TheClickable>
+					<ul class="">
+						<li class="secret-item flex items-center space-x-3 rounded-lg py-1.5 px-2 -mx-1.5">
+							<FolderIcon class="w-6 h-6" />
+							<div>
+								<div>
+									<span>panteric</span>
+								</div>
+							</div>
+						</li>
+						<li class="secret-item flex items-center space-x-3 rounded-lg py-1.5 px-2 -mx-1.5">
+							<FolderIcon class="w-6 h-6" />
+							<div>
+								<div>
+									<span>odekolon-shop</span>
+								</div>
+							</div>
+						</li>
+						<li class="secret-item flex items-center space-x-3 rounded-lg py-1.5 px-2 -mx-1.5">
+							<FolderIcon class="w-6 h-6" />
+							<div>
+								<div>
+									<span>smolsk</span>
+								</div>
+							</div>
+						</li>
+						<li class="secret-item flex items-center space-x-3 rounded-lg py-1.5 px-2 -mx-1.5">
+							<FolderIcon class="w-6 h-6" />
+							<div>
+								<div>
+									<span>pstroybaza</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</ContentTile>
 			<ContentTile class="w-full">
 				<SecretView />
 			</ContentTile>
@@ -31,6 +119,7 @@ import TextField from '~/components/ui/TextField.vue'
 import TextArea from '~/components/ui/TextArea.vue'
 import Sidebar from '~/components/account/LeftSidebarWithGroups.vue'
 import GearIcon from '~/assets/svg/Monochrome=gearshape.fill.svg'
+import FolderIcon from '~/assets/svg/Colored=folder.fill.svg'
 import SecretView from '~/components/account/secrets/View.vue'
 import { definePageMeta, useHead } from '#imports'
 
@@ -40,4 +129,20 @@ useHead({ title: 'Cerber - Доступы' })
 
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+.secret-item {
+	&:hover {
+		background-color: theme('colors.gray.50');
+
+		.secret-icon {
+			// background-color: theme('colors.gray.150');
+
+			svg {
+				// color: theme('colors.gray.900');
+			}
+		}
+	}
+}
+
+</style>
