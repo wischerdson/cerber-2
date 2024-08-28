3e1247477aafc64c032145e11404a2571dc80c0a
🔥 Осмухин Даниил <osmuhin@web-canape.com>
2024-08-28 15:09:18 +0300

    Small fixes in request plugin

--- client/plugins/request.ts
+++ client/plugins/request.ts
@@ -1,34 +1,19 @@
 import { defineNuxtPlugin } from 'nuxt/app'
-import { encryptRequestDecorator, decryptResponseDecorator, type EncryptDecoratedRequest } from '~/decorators/crypt'
-import { authRequestDecorator, type AuthDecoratedRequest } from '~/decorators/auth'
-import { makeRequest, type AppRequest } from '~/utils/request'
-
-// type DecoratedRequest<DataT, RequestT extends AppRequest = AppRequest<DataT>> =
-// 	RequestT &
-// 	EncryptDecoratedRequest<DecoratedRequest<DataT, RequestT>> &
-// 	AuthDecoratedRequest<RequestT>
+import { encryptRequestDecorator, decryptResponseDecorator } from '~/decorators/crypt'
+import { authRequestDecorator } from '~/decorators/auth'
+import { makeRequest } from '~/utils/request'
 
 type Args<DataT> = Parameters<typeof makeRequest<DataT>>
 
 export default defineNuxtPlugin(async () => {
-	const decorators = [
-		decryptResponseDecorator,
-		encryptRequestDecorator,
-		authRequestDecorator
-	]
-
 	const factory = <DataT>(...args: Args<DataT>) => {
 		return authRequestDecorator(
 			encryptRequestDecorator(
 				decryptResponseDecorator(
-					makeRequest(...args)
+					makeRequest<DataT>(...args)
 				)
 			)
 		)
-		// return decorators.reduce(
-		// 	(request, decorator) => decorator(request),
-		// 	makeRequest(...args)
-		// ) as DecoratedRequest<DataT>
 	}
 
 	return {
