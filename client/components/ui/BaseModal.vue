b7cbda8c67ef1d34cc9ae88b42472115d93a73e0
Осмухин Даниил <wischerdson@gmail.com>
2024-11-03 21:04:52 +0300

    UI modal window

---
+++ client/components/ui/BaseModal.vue
@@ -0,0 +1,66 @@
+<template>
+	<ClientOnly>
+		<teleport to="body">
+			<transition
+				@before-leave="emit('before-leave')"
+				@after-leave="emit('after-leave')"
+				@before-enter="emit('before-enter')"
+				@after-enter="emit('after-enter')"
+			>
+				<div class="ui-modal fixed inset-0 z-30" v-if="show">
+					<div class="overflow-y-auto overscroll-contain absolute inset-0 -z-20">
+						<div class="min-h-full flex items-center justify-center relative -z-20 py-14 lg:py-0">
+							<div class="ui-modal__darken-bg fixed inset-0 bg-black/60 -z-10" @click="show = false"></div>
+							<div class="ui-modal__content">
+								<slot></slot>
+							</div>
+						</div>
+					</div>
+				</div>
+			</transition>
+		</teleport>
+	</ClientOnly>
+</template>
+
+<script setup lang="ts">
+
+const show = defineModel<boolean>({ required: true })
+
+export type ModalEmits = {
+	(e: 'before-leave'): void
+	(e: 'after-leave'): void
+	(e: 'before-enter'): void
+	(e: 'after-enter'): void
+}
+
+const emit = defineEmits<ModalEmits>()
+
+</script>
+
+<style lang="scss">
+
+.ui-modal {
+	&.v-enter-active, &.v-leave-active {
+		transition: opacity .3s ease;
+
+		.ui-modal__darken-bg {
+			transition: opacity .3s ease;
+		}
+
+		.ui-modal__content {
+			transition: opacity .25s ease, transform .3s cubic-bezier(.23,.13,.15,1);
+		}
+	}
+
+	&.v-enter-from, &.v-leave-to {
+		.ui-modal__darken-bg, .ui-modal__content {
+			opacity: 0;
+		}
+	}
+
+	&.v-enter-from .ui-modal__content {
+		transform: scale(.85);
+	}
+}
+
+</style>
---
+++ client/components/ui/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+	<UiBaseModal v-model="show">
+		<div v-bind="useAttrs()" class="bg-white relative p-6 lg:px-4 rounded-2xl lg:rounded-none overflow-auto">
+			<button
+				class="btn w-10 h-10 p-0 z-10 absolute lg:fixed right-0 top-0 opacity-50 transition-opacity hover:opacity-100"
+				@click="show = false"
+				title="Закрыть"
+			>
+				<icon name="material-symbols:close-rounded" size="22px" />
+			</button>
+			<slot></slot>
+		</div>
+	</UiBaseModal>
+</template>
+
+<script setup lang="ts">
+
+import { useAttrs } from 'vue'
+import UiBaseModal from '~/components/ui/BaseModal.vue'
+
+const show = defineModel<boolean>({ required: true })
+
+</script>
