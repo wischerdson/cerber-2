d56b65ff39999876a3aaa52977c4417c73864f3b
Осмухин Даниил <wischerdson@gmail.com>
2025-02-04 15:23:48 +0300

    Setting tailwindcssa

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,5 +1,30 @@
+import svgLoader from 'vite-svg-loader'
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
-	devtools: { enabled: true }
+	devtools: { enabled: false },
+	vite: {
+		plugins: [
+			svgLoader({ defaultImport: 'component' }),
+			tailwindcss()
+		]
+	},
+	css: [
+		'~/assets/css/fonts.css',
+		'~/assets/css/tailwind.css'
+	],
+	components: false,
+	app: {
+		rootId: 'app',
+		buildAssetsDir: process.env.NODE_ENV === 'production' ? '/assets/' : void 0,
+	},
+	imports: {
+		autoImport: false
+	},
+	modules: [
+		'nuxt-icon',
+		'@pinia/nuxt'
+	]
 })
